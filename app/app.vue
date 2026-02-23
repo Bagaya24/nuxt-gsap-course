cd291b95daa7aeb78347610b7c1dbcefd4f25b41
bagaya <bagayafazili@gmail.com>
2026-02-23 13:49:01 +0200

    initialisation

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage/>
+  </div>
+</template>
+
+<script setup>
+  import gsap from "gsap";
+import {ScrollTrigger, SplitText} from "gsap/all"
+
+  gsap.registerPlugin(ScrollTrigger, SplitText); 
+
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex-center">
+         <h1 class="text-2xl text-red-200 font-black">
+        Hello world
+    </h1>
+    </div>
+   
+</template>
